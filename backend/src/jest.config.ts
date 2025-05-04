42c6bdef047e43c8797329d061def740880a11ca
Anjana Dunukara <anjanaheshanthach@gmail.com>
2025-05-04 10:03:32 +0530

    jest configuration and setup to support type script

---
+++ backend/src/jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFiles: ["<rootDir>/src/tests/setup.ts"],
+  testMatch: ["**/__tests__/**/*.test.ts"],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+};
+
+export default config;
